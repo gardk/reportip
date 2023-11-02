49d990d859bfea636cbc088926cc324edc88e6f4
Gard Kylling <gard@kylling.io>
2023-11-02 13:11:44 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::{env, net::SocketAddr};
+
+use anyhow::{Context, Error as Anyhow};
+use axum::{extract::ConnectInfo, routing::get, Router};
+use tracing_subscriber::fmt::time::UtcTime;
+
+fn main() -> anyhow::Result<()> {
+    let addr: SocketAddr = env::var("ADDR")
+        .map_err(Anyhow::from)
+        .and_then(|s| s.parse().map_err(Anyhow::from))
+        .context("Unable to determine address from environment")?;
+
+    tracing_subscriber::fmt()
+        .json()
+        .with_file(false)
+        .with_line_number(false)
+        .with_target(false)
+        .with_current_span(false)
+        .with_span_list(true)
+        .with_timer(UtcTime::rfc_3339())
+        .init();
+
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let app = Router::new().route("/", get(report_ip));
+
+    rt.block_on(async move {
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+            .await
+            .map_err(Into::into)
+    })
+}
+
+async fn report_ip(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
+    tracing::info!(client_addr = %addr, "got connection");
+    format!("{}\n", addr.ip())
+}
